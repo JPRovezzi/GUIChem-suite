0ebda5cc1796822ae74c942a00ffc89de6769290
JPRovezzi <100390644+JPRovezzi@users.noreply.github.com>
2024-12-11 11:23:35 -0300

    default

---
+++ addons/default/frame_save.py
@@ -0,0 +1,15 @@
+'''This module contains the logic to save the image and the data in the specified format'''
+import tkinter as tk
+import os
+# import shutil module to copy files
+import shutil
+# import filedialog from tkinter module to save files
+from tkinter import filedialog
+# import the main frame
+import modules.main_frame.frame_root as frameRoot
+
+def load():
+    return None
+
+def save_data(file_format):
+    return None
