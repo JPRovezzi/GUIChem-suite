f4bd48a8cea452174769438218e48710c88c2e43
JPRovezzi <100390644+JPRovezzi@users.noreply.github.com>
2024-12-11 11:26:11 -0300

    pylint

--- addons/ugropygui/frame_save.py
+++ addons/ugropygui/frame_save.py
@@ -1,13 +1,16 @@
+'''This module contains the logic to save the image and the data in the specified format'''
 import tkinter as tk
 import os
-import modules.main_frame.functions as functions
-import modules.main_frame.frame_root as frameRoot
-import modules.image_handler as imageHandler
-import addons.ugropygui.frame_result as frameResult
 # import shutil module to copy files
 import shutil
 # import filedialog from tkinter module to save files
 from tkinter import filedialog
+# import the main frame
+import modules.main_frame.frame_root as frameRoot
+# import the image handler module
+import modules.image_handler as imageHandler
+
+
 
 def save_image(file_format):
     '''Implement the logic to save the image in the specified file_format'''
@@ -35,71 +38,63 @@ def save_data(file_format):
 
 
 def load():
-        save_window = tk.Toplevel(frameRoot.root)
-        save_window.resizable(0, 0)  # Disable resizing
-        save_window.title("Save")
-        save_window.transient(frameRoot.root)
-        save_window.grab_set()
-        #save_window.geometry("400x300")
-        
-        # Display the output PNG as a widget
-        
-        if os.path.exists("output.png"):
-            imageHandler.insert_image(save_window, "output.png")
-        else:
-             save_window.destroy()
-             return
+    '''Create the save window and display the image to save'''
+    save_window = tk.Toplevel(frameRoot.root)
+    save_window.resizable(0, 0)  # Disable resizing
+    save_window.title("Save")
+    save_window.transient(frameRoot.root)
+    save_window.grab_set()
 
-        
-        # Entry box to write the path to save
-        #tk.Label(save_window, text="Save Path:").pack(pady=5)
-        #path_entry_text = tk.StringVar()
-        #path_entry = tk.Entry(save_window, width=50, textvariable=path_entry_text)
-        #path_entry_text.set("C:/")
-        #path_entry.pack(pady=5)
-        
-        # Selection box to select if it is a SVG image or a PNG image
-        tk.Label(save_window, text="Select picture format:").pack(pady=5)
-        format_var = tk.StringVar(value="png")
-        
-        # Frame to hold the radio buttons
-        radioButton_frame = tk.Frame(save_window)
-        radioButton_frame.pack(pady=10)
+    # Display the output PNG as a widget
 
-        tk.Radiobutton(
-                radioButton_frame, 
-                text="PNG", 
-                variable = format_var, 
-                value="png"
-                ).pack(side=tk.LEFT,padx=0)
-        tk.Radiobutton(
-                radioButton_frame, 
-                text="SVG", 
-                variable = format_var, 
-                value = "svg"
-                ).pack(side=tk.LEFT,padx=0)
-               
-        # Frame to hold the buttons
-        button_frame = tk.Frame(save_window)
-        button_frame.pack(pady=10)
+    if os.path.exists("output.png"):
+        imageHandler.insert_image(save_window, "output.png")
+    else:
+        save_window.destroy()
+        return
 
-        # Save button
-        tk.Button(
-            button_frame, 
-            text="Save picture", 
-            command=lambda: save_image(format_var.get())
-            ).pack(side=tk.LEFT, padx=5)
+    # Selection box to select if it is a SVG image or a PNG image
+    tk.Label(save_window, text="Select picture format:").pack(pady=5)
+    format_var = tk.StringVar(value="png")
+
+    # Frame to hold the radio buttons
+    radiobutton_frame = tk.Frame(save_window)
+    radiobutton_frame.pack(pady=10)
+
+    tk.Radiobutton(
+            radiobutton_frame,
+            text="PNG",
+            variable = format_var,
+            value="png"
+            ).pack(side=tk.LEFT,padx=0)
+    tk.Radiobutton(
+            radiobutton_frame,
+            text="SVG",
+            variable = format_var,
+            value = "svg"
+            ).pack(side=tk.LEFT,padx=0)
+
+    # Frame to hold the buttons
+    button_frame = tk.Frame(save_window)
+    button_frame.pack(pady=10)
+
+    # Save button
+    tk.Button(
+        button_frame,
+        text="Save picture",
+        command=lambda: save_image(format_var.get())
+        ).pack(side=tk.LEFT, padx=5)
 
 
-        tk.Button(
-            button_frame, 
-            text="Save data", 
-            command=lambda: save_data(["txt", "xml", "session"])
-            ).pack(side=tk.LEFT, padx=5)
-        
-        # Cancel button
-        tk.Button(
-            button_frame, 
-            text="Close", 
-            command=lambda: save_window.destroy()
-            ).pack(side=tk.LEFT, padx=5)
+    tk.Button(
+        button_frame,
+        text="Save data",
+        command=lambda: save_data(["txt", "xml", "session"])
+        ).pack(side=tk.LEFT, padx=5)
+
+    # Cancel button
+    tk.Button(
+        button_frame,
+        text="Close",
+        command=lambda: save_window.destroy()
+        ).pack(side=tk.LEFT, padx=5)
