c1bc71c952e1918ab3f8bef2c063709e891b29ea
Juan Pablo Rovezzi <juanrovezzi@hotmail.com>
2025-02-07 17:16:14 -0300

    Copy and select all functions added to TextEntry

--- modules/widget_classes.py
+++ modules/widget_classes.py
@@ -49,8 +49,11 @@ class TextEntry(ctk.CTkEntry):
          # Create labels and commands
         self.bind('<Button-3>',self.popup) # Bind a func to right click
         self.menu = tk.Menu(self,tearoff=0) # Create a menu
+        self.menu.add_command(label='Select all',command=self.select_all)
+        self.menu.add_command(label='Copy',command=self.copy)
         self.menu.add_command(label='Paste',command=self.paste)
         self.menu.add_command(label='Clear',command=self.clear)
+        
 
     def popup(self,event):
         ''' This function creates a popup menu when the user right-clicks on
@@ -70,6 +73,18 @@ class TextEntry(ctk.CTkEntry):
         ''' This function clears the text in the textbox.'''
         self.delete(0, tk.END)
 
+    def copy(self):
+        ''' This function copies the text in the textbox to the clipboard.'''
+        #inp = root.get() # Get the text inside entry widget
+        self.clipboard_clear() # Clear the tkinter clipboard
+        try:
+            text = self.selection_get() # Get the selected text
+        except tk.TclError:
+            text = self.get() # if no text is selected, copy all text
+        self.clipboard_append(text) # Append to system clipboard
+    def select_all(self):
+        ''' This function selects all the text in the textbox.'''
+        self.select_range(0, tk.END)
 class CopyTextBox(ctk.CTkTextbox):
     ''' This class is a custom textbox widget that is allowed to copy the text'''
     def __init__(self, parent, **kwargs):
