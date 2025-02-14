20cbb26a875932c2e1b7311b62fa0be97e80cfca
Juan Pablo Rovezzi <juanrovezzi@hotmail.com>
2025-02-14 12:37:03 -0300

    WiP Composition Table

--- addons/flashcalc/frame_classes.py
+++ addons/flashcalc/frame_classes.py
@@ -13,6 +13,8 @@ from PIL import Image
 # Import the required third-party libraries:
 # CustomTkinter is a custom GUI library for Python.
 import customtkinter as ctk
+# CTkXYFrame is a custom frame with XY scrollbars for Python.
+from modules.ctk_xyframe import CTkXYFrame
 # widget_classes is a module that provides classes for GUI widgets.
 import modules.widget_classes as widget_classes
 
@@ -254,8 +256,6 @@ class WorkSheetFrame2(FlashCalcFrame):
 
         # First row of buttons: Open, Reset
         buttonrow1_frame = ctk.CTkFrame(self)
-        open_button = ctk.CTkButton(
-            buttonrow1_frame, text="Open", cursor="hand2", command=self.open)
         reset_button = ctk.CTkButton(
             buttonrow1_frame, text="Reset", cursor="hand2", command=lambda: self.master.load_module(self.tool, "WorkSheetFrame1"))
 
@@ -269,34 +269,22 @@ class WorkSheetFrame2(FlashCalcFrame):
         parameter_table_label = ctk.CTkLabel(
             cfg1_frame, text=f"Parameter table: {self.parameter_table}")
 
-        # Second row of configuration widgets: 
-        # number of components, number of flash calculations
-        cfg2_frame = ctk.CTkFrame(self)
-        nc_label = ctk.CTkLabel(
-            cfg2_frame, text="Number of components:")
-        nc_spinbox = tk.Spinbox(cfg2_frame, from_=1, to=10, width=5)
-        nf_label = ctk.CTkLabel(
-            cfg2_frame, text="Number of flash calculations:")
-        nf_spinbox = tk.Spinbox(cfg2_frame, from_=1, to=10, width=5)
-
         # Second row of buttons: Show Composition Table, Show Flash Table
         buttonrow2_frame = ctk.CTkFrame(self)
         showct_button = ctk.CTkButton(
-            buttonrow2_frame, text="Show the composition table", cursor="hand2")
-        showft_button = ctk.CTkButton(
-            buttonrow2_frame, text="Show the flash table", cursor="hand2")
-
-        # Comment frame
-        comment_frame = ctk.CTkFrame(self)
-        comment_label = ctk.CTkLabel(comment_frame, text="Comments:")
-        comment_text = widget_classes.TextEntry(comment_frame, width=400, height=5)
+            buttonrow2_frame, text="Edit the composition table", cursor="hand2", command=lambda: CompositionTableWindow(self.master))
 
         # Last row of buttons: Save, Run, Back, Close
         buttonrow3_frame = ctk.CTkFrame(self)
-        save_button = ctk.CTkButton(
-            buttonrow3_frame, text="Save", cursor="hand2", command=self.save)
-        close_button = ctk.CTkButton(
-            buttonrow3_frame, text="Close", cursor="hand2", command=self.master.close_module)
+        next_button = ctk.CTkButton(
+            buttonrow3_frame, text="Next", cursor="hand2", command=lambda: 
+            self.master.load_module(
+                self.tool,
+                "WorkSheetFrame3",
+                error_message="",
+                problem_name = self.problem_name,
+                model = self.model,
+                parameter_table = self.parameter_table))
         back_button = ctk.CTkButton(
             buttonrow3_frame, text="Back", cursor="hand2", command=lambda: 
             self.master.load_module(
@@ -306,9 +294,7 @@ class WorkSheetFrame2(FlashCalcFrame):
                 problem_name = self.problem_name,
                 model = self.model,
                 parameter_table = self.parameter_table))
-        run_button = ctk.CTkButton(
-            buttonrow3_frame, text="Run", cursor="hand2", command=lambda: self.master.load_module(self.tool, "ResultsFrame"))
-
+        
         # Add the widgets to the frame with the pack method
         # Title and picture
         title.pack(side="left", padx = 5)
@@ -322,37 +308,27 @@ class WorkSheetFrame2(FlashCalcFrame):
 
         # First row of configuration widgets: 
         # problem name, model, parameter table
-        problem_name_label.pack(side="left", padx=5)
-        model_label.pack(side="left", padx=5)
-        parameter_table_label.pack(side="left", padx=5)
-        cfg1_frame.pack(pady=10,fill="x")
+        problem_name_label.grid(row=0, column=0, padx=5)
+        model_label.grid(row=0, column=1, padx=5)
+        parameter_table_label.grid(row=0, column=2, padx=5)
+        cfg1_frame.pack(pady=10)
 
-        # Second row of configuration widgets:
-        # number of components, number of flash calculations
-        nc_label.pack(side="left", padx=5)
-        nc_spinbox.pack(side="left", padx=5)
-        nf_label.pack(side="left", padx=5)
-        nf_spinbox.pack(side="left", padx=5)
-        cfg2_frame.pack(pady=10,fill="x")
-        
         # Second row of buttons: Show Composition Table, Show Flash Table
-        showct_button.pack(side="left", padx=5)
-        showft_button.pack(side="left", padx=5)
-        buttonrow2_frame.pack(pady=10,fill="x")
+        showct_button.grid(row=0, column=0, padx=5)
+        ctk.CTkButton(
+            buttonrow2_frame,text="",hover=False).grid(row=0, column=1, padx=5)
+        buttonrow2_frame.pack(pady=10)
 
-        # Comment frame
-        comment_label.pack(padx=5, side = "left")
-        comment_text.pack(padx=5,side="left")
-        comment_frame.pack(pady=10,fill="x")
-
-        # Third and last row of buttons: Save, Run, Back, Close
-        back_button.pack(side="left", padx=5)
-        ctk.CTkLabel(buttonrow3_frame, text="  |  ").pack(side="left", padx=10)
-        save_button.pack(side="left", padx=5)
-        run_button.pack(side="left", padx=5)
-        ctk.CTkLabel(buttonrow3_frame, text="  |  ").pack(side="left", padx=10)
-        close_button.pack(side="left", padx=5)
-        buttonrow3_frame.pack(pady=50, side="bottom",fill="x")
+        # Last row of buttons: Back, Next
+        back_button.grid(row=0, column=0, padx=5)
+        ctk.CTkLabel(buttonrow3_frame, text="  |  ").grid(row=0, column=1, padx=5)
+        ctk.CTkButton(
+            buttonrow3_frame,text="",hover=False).grid(row=0, column=2, padx=5)
+        ctk.CTkButton(
+            buttonrow3_frame,text="",hover=False).grid(row=0, column=3, padx=5)
+        ctk.CTkLabel(buttonrow3_frame, text="  |  ").grid(row=0, column=4, padx=5)
+        next_button.grid(row=0, column=5, padx=5)
+        buttonrow3_frame.pack(pady=50, side="bottom")
 
         # Pack the worksheet frame
         if os.name == 'nt':
@@ -498,4 +474,53 @@ class WorkSheetFrame3(FlashCalcFrame):
 
         self.pack(pady=0, expand=True, fill="both")
         return None
+
+class CompositionTableWindow(ctk.CTkToplevel):
+    '''Class to create the composition table window.'''
     
+    def __init__(self, master, **kwargs):
+        '''Initialize the class.'''
+        super().__init__(master, **kwargs)
+        self.title("Composition Table")
+        self.geometry("600x400")
+        
+        self.table = []
+        self.table_frame = CTkXYFrame(self)
+        self.table_frame.pack(fill="both", expand=True, padx=10, pady=10)
+        
+        self.add_row_button = ctk.CTkButton(
+            self.table_frame, text="Add Component", cursor="hand2", command=self.add_row)
+        self.add_row_button.grid(row=0, column=0, padx=5, pady=5)
+        
+        self.subtract_row_button = ctk.CTkButton(
+            self.table_frame, text="Subtract Component", cursor="hand2", command=self.subtract_row)
+        self.subtract_row_button.grid(row=0, column=1, padx=5, pady=5)
+
+        self.row_count_label = ctk.CTkLabel(
+            self.table_frame, text=f"Components: {int(len(self.table)/2)}")
+        self.row_count_label.grid(row=0, column=2, padx=5, pady=5)
+        
+        self.add_row()
+    
+    def add_row(self):
+        '''Add a pair of rows to the table.'''
+        for i in range(2):
+            row = []
+            for col in range(11):  # Assuming 11 columns for the table
+                entry = widget_classes.TextEntry(self.table_frame)
+                entry.grid(row=len(self.table) + 1, column=col, padx=5, pady=5)
+                row.append(entry)
+            self.table.append(row)
+        self.row_count_label.configure(
+            text=f"Components: {int(len(self.table)/2)}")
+
+    
+    def subtract_row(self):
+        '''Subtract a pair of rows from the table.'''
+        if self.table:
+            for i in range(2):
+                row = self.table.pop()
+                for entry in row:
+                    entry.destroy()
+        self.row_count_label.configure(
+            text=f"Components: {int(len(self.table)/2)}")
